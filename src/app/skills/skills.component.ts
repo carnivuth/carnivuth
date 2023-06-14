b29cdc96a79bd4d0922953b65037753b64caf6ae
carnivuth <matti200042@gmail.com>
2023-06-14 23:18:15 +0200

    added skill html and logix

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DatabrokerService } from '../databroker.service';
+import { Skill } from '../model/Skill';
 
 @Component({
   selector: 'app-skills',
   templateUrl: './skills.component.html',
-  styleUrls: ['./skills.component.scss']
+  styleUrls: ['./skills.component.scss'],
 })
-export class SkillsComponent {
-
+export class SkillsComponent  implements OnInit  {
+  skills?: Skill[];
+  constructor(private db: DatabrokerService) {}
+  ngOnInit() {
+    this.skills = this.db.skills;
+  }
 }
