dcb67e216b2c9e89ec8d2eac9ed3643690caa37a
carnivuth <matti200042@gmail.com>
2023-06-14 23:46:17 +0200

    added wor experiences component

--- src/app/work-experiences/work-experiences.component.ts
+++ src/app/work-experiences/work-experiences.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DatabrokerService } from '../databroker.service';
+import { WorkExperience } from '../model/WorkExperience';
 
 @Component({
   selector: 'app-work-experiences',
   templateUrl: './work-experiences.component.html',
   styleUrls: ['./work-experiences.component.scss']
 })
-export class WorkExperiencesComponent {
-
+export class WorkExperiencesComponent implements OnInit
+{
+  experiences?:WorkExperience[];
+  constructor(private db: DatabrokerService) {}
+  
+  ngOnInit(): void {
+    this.experiences=this.db.workExperiences;
+  }
 }
