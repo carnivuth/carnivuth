8bfb1bd6dcb40317aab0f334aa11b963c16a5319
carnivuth <matti200042@gmail.com>
2023-06-14 23:45:30 +0200

    added model elements

--- src/app/databroker.service.ts
+++ src/app/databroker.service.ts
@@ -1,15 +1,25 @@
 import { Injectable } from '@angular/core';
 import portfolio from '../assets/portfolio.json';
 import { PersonalData } from './model/PersonalData';
+import { Skill } from './model/Skill';
+import { ValueFromNotification } from 'rxjs';
+import { Formation } from './model/Formation';
+import { WorkExperience } from './model/WorkExperience';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DatabrokerService {
   pd:PersonalData;
+  skills:Skill[];
+  formations:Formation[];
+  workExperiences:WorkExperience[];
 
   constructor() { 
     this.pd=portfolio.personalData;
+    this.skills=portfolio.skills;
+    this.workExperiences=portfolio.workexperiences;
+    this.formations=portfolio.formation;
   }
   
 }
