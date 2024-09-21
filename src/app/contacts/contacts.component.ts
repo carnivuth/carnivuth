a2a4b265a8945dfe31b91a498f54d03625d5f063
carnivuth <matti200042@gmail.com>
2024-09-21 14:31:51 +0200

    added import

--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DatabrokerService } from '../databroker.service';
 import { Contact } from '../model/Contact';
+import { Location, LocationStrategy } from '@angular/common';
 
 @Component({
   selector: 'app-contacts',
