9680c8dc9362bf69da594e4e9a7d3f7f961d45b2
carnivuth <matti200042@gmail.com>
2026-03-22 17:06:06 +0100

    refactor script using cli.sh from scripts repo

--- check.sh
+++ check.sh
@@ -26,6 +26,7 @@ function count_drafts(){
   echo there are $( list_drafts | wc -l) notes in draft
 }
 function lint_fm(){
+  test -z $DEBUG || echo "Linting front matter for all markdown files in $CONTENT_DIR"
   find $CONTENT_DIR -type f -name '*.md' -exec yq --front-matter=process -i 'sort_keys(.)' {} \;
 }
 
@@ -50,51 +51,81 @@ function list_missing_title(){
   grep -L -e '^title: .*' $CONTENT_DIR/*
 }
 
-function list_escaped_title(){
-  grep -l -e '^title: ".*"' $CONTENT_DIR/*
-}
-
 function list_broken_slug(){
   find $CONTENT_DIR -type f -name '*.md' | while read file; do
   grep -q "slug: $(basename $file)" "$file" || echo "$file"
 done
 }
 
-case "$1" in
-  count_drafts)
-    count_drafts
-    ;;
-  list_drafts)
-    list_drafts
-    ;;
-  list_broken_links)
-    list_broken_links
-    ;;
-  list_missing_description)
-    list_missing_description
-    ;;
-  list_escaped_title)
-    list_escaped_title
-    ;;
-  list_missing_title)
-    list_missing_title
-    ;;
-  list_broken_slug)
-    list_broken_slug
-    ;;
-  list_tags)
-    get_fm_param '.tags[]' | sort -u
-    ;;
-  set_aliases)
-    set_aliases
-    ;;
-  lint_fm)
-    lint_fm
-    ;;
-  list_broken)
+function pre_commit(){
+  DEBUG=true set_aliases
+  DEBUG=true lint_fm
+}
+
+function setup_repo_hooks(){
+  mkdir -p .git/hooks
+  echo "Setting up git hooks"
+  echo -e '#!/bin/bash\n./check.sh pre_commit' > .git/hooks/pre-commit
+  chmod +x .git/hooks/pre-commit
+}
+
+function list_tags(){
+  get_fm_param '.tags[]' | sort -u
+}
+
+function list_broken(){
     list_broken_slug
     list_drafts
     list_broken_links
     list_missing_description
-    ;;
-esac
+}
+
+
+function help(){
+  echo "usage: $(basename "$0") COMMAND [$FLAGS_STRING]"
+  echo "list of commands:"
+  for command in "${!COMMANDS[@]}"; do
+    echo "       $command -> ${COMMANDS[$command]}"
+  done
+  echo "list of flags:"
+  for flag in "${!FLAGS_DESCRIPTIONS[@]}"; do
+    echo "       -$flag -> ${FLAGS_DESCRIPTIONS[$flag]}"
+  done
+}
+
+declare -A FLAGS
+declare -A FLAGS_DESCRIPTIONS
+declare -A COMMANDS
+
+COMMANDS[help]="show this help command"
+COMMANDS["set_aliases"]="set aliases in markdown files $CONTENT_DIR"
+COMMANDS["list_drafts"]="list notes with draft:true in frontmatter"
+COMMANDS["count_drafts"]="count draft notes under $CONTENT_DIR"
+COMMANDS["lint_fm"]="lint markdown front matter in $CONTENT_DIR by sorting keys"
+COMMANDS["list_broken_links"]="list broken links in files under $CONTENT_DIR"
+COMMANDS["list_missing_description"]="list notes without description"
+COMMANDS["list_missing_title"]="list notes without title"
+COMMANDS["list_broken_slug"]="list notes with slug not matching 'basename file'"
+COMMANDS["pre_commit"]="run pre-commit actions"
+COMMANDS["setup_repo_hooks"]="setup git hooks"
+COMMANDS["list_broken"]="list broken content"
+COMMANDS["list_tags"]="list tag values"
+
+
+COMMAND="$1"
+shift
+if [[ $COMMAND  == '' ]] || [[ $COMMAND  == -* ]]; then echo "first parameter must be a command"; help; exit 1; fi
+
+if [[ ! -z $FLAGS_STRING ]];then
+  while getopts $FLAGS_STRING flag; do
+    [ "${FLAGS[$flag]}" ] && eval ${FLAGS[$flag]}
+  done
+fi
+
+if [ "${COMMANDS[$COMMAND]}" ]; then
+  "$COMMAND"
+else
+  echo "$COMMAND not exists"
+  help
+  exit 1
+fi
