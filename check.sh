f308fe2e6d02446a06fdb3976bad8d86806531e3
carnivuth <matti200042@gmail.com>
2026-04-06 20:07:51 +0200

    added image setup from igdb, minor improvements on the script with parameters

--- check.sh
+++ check.sh
@@ -89,22 +89,35 @@ function list_broken(){
 
 function get_igdb_data(){
 
-grep -l 'igdb_id:' content/*.md | while read f; do
+# loop files with igdb_id in front matter
+grep -l 'igdb_id:' $CONTENT_DIR/*.md | while read f; do
 
   echo "processing $f"
   igdb_id=$(yq --front-matter=export '.igdb_id' $f);
-  game_data="$(curl 'https://api.igdb.com/v4/games' -d "fields *; where id=$igdb_id;" -H "Client-ID: $IGDB_CLIENT_ID" -H "Authorization: Bearer $IGDB_ACCESS_TOKEN" -H 'Accept: application/json')";
+  game_data="$(curl -s 'https://api.igdb.com/v4/games' -d "fields *; where id=$igdb_id;" -H "Client-ID: $IGDB_CLIENT_ID" -H "Authorization: Bearer $IGDB_ACCESS_TOKEN" -H 'Accept: application/json')";
+  game_name="$(echo $game_data| jq -r '.[0].name' )"
+
+  # setting title
+  echo "setting title for  $f to $game_name"
+  yq --front-matter=process ".title = \"$game_name\"" -i $f
 
   # parse genres and update front matter only if they are unset
   if [[ $(yq --front-matter=export '.genres' $f) == "null"  ]]; then
-    echo "getting genres for $f"
+    echo "getting genres for $game_name"
     genre_ids="$(echo $game_data | jq '.[0].genres[]' -r)";
-    genres_data="$(curl 'https://api.igdb.com/v4/genres' -d "fields *; where id=\($(echo $genre_ids | tr ' ' ',')\);" -H "Client-ID: $IGDB_CLIENT_ID" -H "Authorization: Bearer $IGDB_ACCESS_TOKEN" -H 'Accept: application/json')"
+    genres_data="$(curl -s 'https://api.igdb.com/v4/genres' -d "fields *; where id=\($(echo $genre_ids | tr ' ' ',')\);" -H "Client-ID: $IGDB_CLIENT_ID" -H "Authorization: Bearer $IGDB_ACCESS_TOKEN" -H 'Accept: application/json')"
     yq --front-matter=process ".genres = $(echo $genres_data| jq '.|[.[].name]')" -i $f
   fi
 
-  # this is to avoid hitting rate limits
-  sleep 1
+  if [[ $(yq --front-matter=export '.image' $f) == ""  ]]; then
+    echo "getting cover for $game_name"
+    image_id="$(curl -s 'https://api.igdb.com/v4/covers' -d "fields *; where game=$igdb_id;" -H "Client-ID: $IGDB_CLIENT_ID" -H "Authorization: Bearer $IGDB_ACCESS_TOKEN" -H 'Accept: application/json' | jq -r '.[0].image_id')"
+    yq --front-matter=process ".image = \"https://images.igdb.com/igdb/image/upload/t_1080p/${image_id}.jpg\"" -i $f
+  fi
+
+  # this is to avoid hitting igdb api rate limits
+  sleep 2
+
 done
 
 }
