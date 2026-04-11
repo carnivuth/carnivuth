c8d9f1fde19513a1353c20766dcb63f633ae6b33
carnivuth <matti200042@gmail.com>
2026-04-11 19:04:38 +0200

    added first version of filters

--- assets/js/games.js
+++ assets/js/games.js
@@ -1,4 +1,33 @@
-function toggle_filters(filter_name){
-    content = document.getElementById(filter_name)
-    content.style.display = content.style.display === 'none' ? '' : 'none'
+function filter(){
+  // get filters and cards
+  genre_filter = document.getElementById('genres')
+  backlog_status_filter = document.getElementById('backlog_statuses')
+  cards = document.getElementById('card-container')
+
+  // check if no filters are selected
+  if (genre_filter.selectedOptions.length == 0 && backlog_status_filter.selectedOptions.length == 0) {
+    for (let card of cards.children) {
+      card.style.display = 'block'
+    }
+  }else{
+    for (let card of cards.children) {
+      selectedByBacklogStatus = backlog_status_filter.selectedOptions.length == 0 ? true : false
+      selectedByGenre = genre_filter.selectedOptions.length == 0 ? true : false
+      for (let option of genre_filter.selectedOptions) {
+        if (card.classList.contains(option.value)) {
+          selectedByGenre = true
+        }
+      }
+      for (let option of backlog_status_filter.selectedOptions) {
+        if (card.classList.contains(option.value)) {
+          selectedByBacklogStatus = true
+        }
+      }
+      if (selectedByBacklogStatus && selectedByGenre) {
+        card.style.display = 'block'
+      } else {
+        card.style.display = 'none'
+      }
+    }
+  }
 }
